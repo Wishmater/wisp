c768c5bfdc156a95f5464b57f4b9cb898054e367
David Alcalde <david.alcalde.cu@gmail.com>
2026-05-13 16:32:39 -0500

    ui: add gesture detector to new table implementation

--- lib/ui/files_list.dart
+++ lib/ui/files_list.dart
@@ -37,13 +37,21 @@ class FilesList extends ConsumerWidget {
                 rowHeight: 48,
                 horizontalDetails: ScrollableDetails.horizontal(controller: horizontalController),
                 verticalDetails: ScrollableDetails.vertical(controller: verticalController),
-                delegate: TwoDimensionalChildBuilderDelegate(
+                delegate: FilesChildDelegate(
                   maxXIndex: columns.length - 1,
                   maxYIndex: data.length - 1,
+                  onRowTap: (index) {
+                    final fileData = data[index];
+                    if (fileData is DirectoryData) {
+                      setCurrentDirectory(ref, fileData.path);
+                    } else {
+                      openFile(fileData);
+                    }
+                  },
                   builder: (BuildContext context, ChildVicinity vicinity) {
-                    final type = columns[vicinity.xIndex];
-                    final file = data[vicinity.yIndex];
-                    return Text(file.getStatTypeFormatted(context, type));
+                    final statType = columns[vicinity.xIndex];
+                    final fileData = data[vicinity.yIndex];
+                    return Text(fileData.getStatTypeFormatted(context, statType));
                   },
                 ),
               );
@@ -191,20 +199,74 @@ class RenderFilesListViewport extends RenderTwoDimensionalViewport {
     final int lastRow = (renderEndY / cellHeight).ceil().clamp(0, rowCount - 1);
 
     for (int row = firstRow; row <= lastRow; row++) {
+      // Layout row-wide hit target
+      final RenderBox? rowGesture = buildOrObtainChildFor(RowGestureChildVicinity(yIndex: row));
+      if (rowGesture != null) {
+        final rowGestureParentData = rowGesture.parentData! as TwoDimensionalViewportParentData;
+        // TODO: 3 it probably looks better if we limit this to the viewport with instead of spanning over edges
+        rowGesture.layout(BoxConstraints.tight(Size(maxWidth, cellHeight)));
+        rowGestureParentData.layoutOffset = Offset(
+          0,
+          row * cellHeight - verticalOffset.pixels,
+        );
+      }
+      // Layout row cells
       for (int col = firstCol; col <= lastCol; col++) {
-        final cellWidth = columnSizes[col];
-        final vicinity = ChildVicinity(xIndex: col, yIndex: row);
-        final RenderBox? child = buildOrObtainChildFor(vicinity);
+        final RenderBox? child = buildOrObtainChildFor(ChildVicinity(xIndex: col, yIndex: row));
         if (child == null) continue;
+        final cellWidth = columnSizes[col];
         final parentData = child.parentData! as TwoDimensionalViewportParentData;
-        // Layout the cell with a fixed tight constraint.
         child.layout(BoxConstraints.tight(Size(cellWidth, cellHeight)));
-        // Position the cell relative to the viewport origin.
         parentData.layoutOffset = Offset(
           columnOffsets[col] - horizontalOffset.pixels,
           row * cellHeight - verticalOffset.pixels,
         );
       }
     }
+  }
+}
+
+class RowGestureChildVicinity extends ChildVicinity {
+  const RowGestureChildVicinity({required super.yIndex}) : super(xIndex: -1);
+}
+
+class SelectedRowChildVicinity extends ChildVicinity {
+  final int extent;
+  const SelectedRowChildVicinity({
+    required super.yIndex,
+    required this.extent,
+  }) : assert(extent > 0),
+       super(xIndex: -2);
+}
+
+class FilesChildDelegate extends TwoDimensionalChildBuilderDelegate {
+  void Function(int rowIndex)? onRowTap;
+  void Function(int rowIndex)? onRowDoubleTap;
+
+  FilesChildDelegate({
+    required super.builder,
+    this.onRowTap,
+    this.onRowDoubleTap,
+    super.maxXIndex,
+    super.maxYIndex,
+    super.addRepaintBoundaries,
+    super.addAutomaticKeepAlives,
+  });
+
+  @override
+  Widget? build(BuildContext context, covariant ChildVicinity vicinity) {
+    if (vicinity is RowGestureChildVicinity) {
+      if (onRowTap == null && onRowDoubleTap == null) {
+        return null;
+      }
+      return InkWell(
+        onTap: onRowTap == null ? null : () => onRowTap!(vicinity.yIndex),
+        onDoubleTap: onRowDoubleTap == null ? null : () => onRowDoubleTap!(vicinity.yIndex),
+      );
+    }
+    if (vicinity is SelectedRowChildVicinity) {
+      return null; // TODO: 3 implement selection UI
+    }
+    return builder(context, vicinity);
   }
 }
