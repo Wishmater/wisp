9e9caf74ee4fcf8d1cea00cfeeef8b06fea52e9e
David Alcalde <david.alcalde.cu@gmail.com>
2026-05-15 10:36:40 -0500

    remove annoying fucking GTK warning log

--- linux/runner/main.cc
+++ linux/runner/main.cc
@@ -1,6 +1,23 @@
 #include "my_application.h"
 
+static GLogWriterOutput log_writer(GLogLevelFlags log_level,
+                                   const GLogField* fields, gsize n_fields,
+                                   gpointer user_data) {
+  const gchar* message = nullptr;
+  for (gsize i = 0; i < n_fields; i++) {
+    if (g_strcmp0(fields[i].key, "MESSAGE") == 0) {
+      message = static_cast<const gchar*>(fields[i].value);
+      break;
+    }
+  }
+  if (message && g_str_has_prefix(message, "gdk_device_get_source")) {
+    return G_LOG_WRITER_HANDLED;
+  }
+  return g_log_writer_default(log_level, fields, n_fields, user_data);
+}
+
 int main(int argc, char** argv) {
+  g_log_set_writer_func(log_writer, nullptr, nullptr);
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
